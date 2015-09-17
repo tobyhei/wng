ec453631acfa46491a5e6f18305e92caf3c1aabb
Tobias Heighway <toby_hei@outlook.com>
2015-09-17 23:00:26 +1000

    Add protractor for e2e testing

---
+++ wng/Scripts/conf.js
@@ -0,0 +1,5 @@
+﻿exports.config = {
+    framework: 'jasmine2',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js']
+}
---
+++ wng/Scripts/spec.js
@@ -0,0 +1,17 @@
+﻿describe('Wng Demo App', function () {
+    beforeEach(function () {
+        browser.get('http://localhost:29650/');
+    })
+
+    it('should have a title', function () {
+        expect(browser.getTitle()).toEqual('Numeric Sequence Calculator');
+    });
+
+    it('should take a positive integer', function () {
+        // Find our number input and put in valid data
+        element(by.id('numberInput')).sendKeys('15');
+
+        // Find the submit button and click it
+        element(by.id('submitButton')).submit();
+    });
+});
