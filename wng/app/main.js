549c3570db62d1e16e6d1cc3aef65335b99522c3
Tobias Heighway <toby_hei@outlook.com>
2015-09-17 10:46:15 +1000

    S2 Enter Data
    Add custom validation for checking number is an integer.
    Also change validation error to display only on click

--- wng/app/main.js
+++ wng/app/main.js
@@ -10,4 +10,28 @@
         vm.number = 0;
     }
 
+    var INTEGER_REGEXP = /^\-?\d+$/;
+    angular
+        .module('app').
+            directive('integer', function () {
+                return {
+                    require: 'ngModel',
+                    link: function (scope, elm, attrs, ctrl) {
+                        ctrl.$validators.integer = function (modelValue, viewValue) {
+                            if (ctrl.$isEmpty(modelValue)) {
+                                // consider empty models to be valid
+                                return true;
+                            }
+
+                            if (INTEGER_REGEXP.test(viewValue)) {
+                                // it is valid
+                                return true;
+                            }
+
+                            // it is invalid
+                            return false;
+                        };
+                    }
+                };
+            });
 })();
