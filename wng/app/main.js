18a9945e36a54a5a192f12b32077ec9938ad56b6
Tobias Heighway <toby_hei@outlook.com>
2015-09-17 12:33:37 +1000

    S3 Results
    List the following:
    S3.1.1 All numbers up to and including the number entered,
    S3.1.2 All odd numbers up to and including the number entered,
    S3.1.3 All even numbers up to and including the number entered,
    S3.1.4 All numbers up to and including the number entered, except
    when,
    S3.1.4.1 A number is a multiple of 3 output C, and when,
    S3.1.4.2 A number is a multiple of 5 output E, and when,
    S3.1.4.3 A number is a multiple of both 3 and 5 output Z,
    S3.1.5 All fibonacci number up to and including the number entered
    
    Also hide the input and submit button once correct data has been submitted.

--- wng/app/main.js
+++ wng/app/main.js
@@ -8,6 +8,67 @@
     function main() {
         var vm = this;
         vm.number = 0;
+        vm.all = function () {
+            var val = vm.number;
+            var result = [];            
+            var i;
+            for (i = 1; i <= val; i++) {
+                result[i] = i;
+            }
+            return result.filter(function (n) { return n != undefined });
+        };
+        vm.odd = function () {
+            var val = vm.number;
+            var result = [];
+            var i;
+            for (i = 1; i <= val; i+=2) {
+                result[i] = i;
+            }
+            return result.filter(function (n) { return n != undefined });
+        };
+        vm.even = function () {
+            var val = vm.number;
+            var result = [];
+            var i;
+            for (i = 2; i <= val; i += 2) {
+                result[i] = i;
+            }
+            return result.filter(function (n) { return n != undefined });
+        };
+        vm.fibonacci = function () {
+            var val = vm.number;
+            if (val === 0) return 0;
+            if (val === 1) return 1;
+            if (val === 2) return 1;
+            var fib = [];
+            fib[0] = 0;
+            fib[1] = 1;
+            var i;
+            for (i = 2; i <= val; i++) {                
+                fib[i] = fib[i - 2] + fib[i - 1];
+            }
+            return fib.filter(function (n) { return n != undefined });
+        };
+        vm.fizzBuzz = function () {
+            var val = vm.number;
+            var result = [];
+            var i;
+            for (i = 1; i <= val; i++) {
+                if (i % 3 === 0 && i % 5 === 0) {
+                    result[i] = 'Z';
+                }
+                else if (i % 3 === 0) {
+                    result[i] = 'C';
+                }
+                else if (i % 5 === 0) {
+                    result[i] = 'E';
+                }
+                else {
+                    result[i] = i;
+                }
+            }
+            return result.filter(function (n) { return n != undefined });
+        };
     }
 
     var INTEGER_REGEXP = /^\-?\d+$/;
